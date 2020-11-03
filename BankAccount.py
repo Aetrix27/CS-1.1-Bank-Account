3c31d9d6053aa98c0f744ec1ab3f919181450339
Aetrix27 <davedogg323@gmail.com>
2020-11-02 17:54:03 -0800

    Fixed withdraw function

--- BankAccount.py
+++ BankAccount.py
@@ -10,16 +10,27 @@ class bankAccount:
         return f"Amount Deposited: ${amount}"
     
     def withdraw(self, amount):
-        self.balance -= amount
+        calc_balance = self.balance - amount
+        if calc_balance > self.balance:
+            return "Insufficent Funds!"
+        else:
+            self.balance -= amount
+
         return f"Amount Withdrawn: ${amount}"
 
     def get_balance(self):
         return f"Hello! Your current balance is: {self.balance}"
 
+    def add_interest(self):
+        monthly_interest = self.balance *  0.0008
+        self.balance *= monthly_interest
+        return self.balance
+
 
 
 account1 = bankAccount()
 print(account1.deposit(35))
+account1.add_interest()
 print(account1.get_balance())
 print(account1.withdraw(10))
 print(account1.get_balance())
