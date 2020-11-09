1777c71453d17144b46919d387c28f42a3443274
Aetrix27 <davedogg323@gmail.com>
2020-11-09 00:53:27 -0800

    Made if statements for user menu

--- BankAccount.py
+++ BankAccount.py
@@ -28,6 +28,7 @@ class bankAccount:
         monthly_interest = self.balance *  0.0008
         self.balance *= monthly_interest
         return self.balance
+
     def print_receipt(self):
         return f"""{self.full_name} \n
                 Account No: {self.account_number} \n 
@@ -35,11 +36,24 @@ class bankAccount:
                 Balance: ${self.balance}"""
 
 
+selection = int(input("""Welcome, please select from the options below by typing the number: \n
+        1. Deposit \n
+        2. Withdraw \n
+        3. Get Balance \n
+        4. Add interest \n
+        5. Print Receipt \n"""))
+
 
 account1 = bankAccount()
-print(account1.deposit(35))
-account1.add_interest()
-print(account1.get_balance())
-print(account1.withdraw(10))
-print(account1.get_balance())
-print(account1.print_receipt())
+if(selection == 1):
+    amount = int(input("How much? \n"))
+    print(account1.deposit(amount))
+elif(selection ==2):
+    amount = int(input("How much? \n"))
+    print(account1.withdraw(amount))
+elif(selection == 3):
+    print(account1.get_balance())
+elif(selection == 4):
+    account1.add_interest()
+elif(selection == 5):
+    print(account1.print_receipt())
